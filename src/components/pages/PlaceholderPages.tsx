23a44b4db0c2741cb344c233885ad7ebc20d10bc
tasneemchital <tasneem.chital@simplifyhealthcare.com>
2025-09-25 18:41:24 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/pages/PlaceholderPages.tsx
+++ src/components/pages/PlaceholderPages.tsx
@@ -5476,11 +5476,8 @@ export function Generate() {
                             <Checkbox
                               checked={isAllVisibleSelected}
                               onCheckedChange={handleSelectAll}
-                  <div className="border rounded-lg">
-                    <Table>
-                      <TableHeader>
-                        {/* Column Headers with Sort */}
-                        <TableRow className="bg-muted/50">
+                            />
+                          </TableHead>
                           {visibleColumns.documentName && (
                             <TableHead className="border-r h-10">
                               <div className="flex items-center gap-1 cursor-pointer select-none font-semibold" onClick={() => handleSort('documentName')}>
