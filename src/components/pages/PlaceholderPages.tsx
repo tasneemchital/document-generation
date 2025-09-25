93bd5b79960cfaeb7247c5b0d84d0369d1701321
tasneemchital <tasneem.chital@simplifyhealthcare.com>
2025-09-25 17:27:50 +0000

    Edited Spark

--- src/components/pages/PlaceholderPages.tsx
+++ src/components/pages/PlaceholderPages.tsx
@@ -5097,7 +5097,7 @@ export function Generate() {
         </div>
       </div>
       
-      <Tabs value={currentTab} onValueChange={setCurrentTab} className="w-full">,
+      <Tabs value={currentTab} onValueChange={setCurrentTab} className="w-full">
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="generate-collateral" className="flex items-center gap-2">
             <FileText size={16} />
